be07c706a76debccd0ccb3322e02fa8ec05cbf56
margaretanp <143424663+margaretanp@users.noreply.github.com>
2024-04-25 11:02:05 +0700

    Add files via upload

---
+++ jobsheet 10/Pasien19.java
@@ -0,0 +1,14 @@
+public class Pasien19 {
+    String nama;
+    int noID, umur;
+    char jenisKelamin;
+
+
+
+    public Pasien19(String nama, int noID, char jenisKelamin, int umur) {
+        this.nama = nama;
+        this.noID = noID;
+        this.jenisKelamin = jenisKelamin;
+        this.umur = umur;
+    }
+}
